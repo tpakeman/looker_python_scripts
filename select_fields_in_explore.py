7bb92bbb6f6edd50b2b5c65d55ef44088308a8b6
Tom Pakeman <tom.pakeman@looker.com>
2020-02-24 16:24:44 +0000

    Add colour  coding and make dialect neutral

--- select_fields_in_explore.py
+++ select_fields_in_explore.py
@@ -4,13 +4,27 @@ from looker_sdk.rtl.transport import TransportSettings
 from looker_sdk.error import SDKError
 import argparse
 import re
+import urllib3
+
+# For colour-coding the output
+colours = {'HEADER':'\033[95m',
+           'OKBLUE': '\033[94m',
+           'OKGREEN': '\033[92m',
+           'WARNING': '\033[93m',
+           'FAIL': '\033[91m',
+           'END': '\033[0m',
+           'BOLD': '\033[1m',
+           'UNDERLINE': '\033[4m'}
+
 
 looker_client = client.setup('looker.ini')
 try:
+    # Use this if testing locally or on a server without a certificate
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     user = looker_client.me()
-    print(f"Authenticated as {user.display_name} (User ID {user.id})")
+    print(f"{colours['HEADER']}Authenticated as {user.display_name} (User ID {user.id}){colours['END']}")
 except SDKError:
-    print("Could not authenticate. Check `looker.ini` file")
+    print(f"{colours['FAIL']}Could not authenticate. Check `looker.ini` file{colours['END']}")
 
 
 def get_models():
@@ -23,7 +37,7 @@ def get_explores_from_model(model_name):
     try:
         return [explore.name for explore in looker_client.lookml_model(model_name, fields='explores').explores]
     except SDKError:
-        print(f"ERROR:\tModel {model_name} not found.")
+        print(f"{colours['FAIL']}ERROR:\tModel {model_name} not found.{colours['END']}")
         exit()
 
 
@@ -35,7 +49,7 @@ def get_fields_from_model_explore(model_name, explore_name):
         mes = [mes.name for mes in explore.fields.measures]
         return dims, mes
     except SDKError:
-        print(f"ERROR:\tExplore {explore_name} not found.")
+        print(f"{colours['FAIL']}ERROR:\tExplore {explore_name} not found.{colours['END']}")
         exit()
 
 
@@ -46,72 +60,44 @@ def create_query_from_fields(model_name, explore_name, excludes):
     dims, mes = get_fields_from_model_explore(model_name, explore_name)
     field_set = dims + mes
     field_set = [field for field in field_set if field not in excludes]
-    num_fields = len(field_set)
     body = {"model": model_name, 'view': explore_name, 'fields': field_set}
     query = looker_client.create_query(body)
     del query.client_id
-    return query.id, num_fields, query.share_url
+    return query.id, field_set, query.share_url
 
 
-def check_fields(model_name, explore_name, print_data=False, continue_on_errors=False, excludes=[]):
+def check_fields(model_name, explore_name, continue_on_errors=False, excludes=[]):
     """Runs a query containing all of the fields in a given model and explore to see if there are any SQL errors.
     Can choose to print some helpful output, and to continue if you find an error."""
-    query_id, num_fields, query_url = create_query_from_fields(model_name, explore_name, excludes)
+    query_id, field_set, query_url = create_query_from_fields(model_name, explore_name, excludes)
+    num_fields = len(field_set)
     result = looker_client.run_query(query_id, 'csv', limit=1)
     sql = looker_client.run_query(query_id, 'sql')
-    if result.startswith('SQL Error'):
-        if not continue_on_errors:
-            if 'The query is too large' in result: # This error may be specific to BigQuery.
-                print(result.splitlines()[-1])
-            else:
-                print(result)
-        if 'at [' in result:
-            error_location = result.split(' at [')[1][:-1].split(':')
-            error_line = sql.splitlines()[int(error_location[0]) - 2]
-            if not continue_on_errors:
-                print(f"Error line in SQL: {error_line}")
-                if print_data:
-                    print(query_url)
-            else:
-                if excludes == []:
-                    print("Errors found, continuing...")
-                search_pattern = r'AS ([\w_]*),'
-                matches = [m.lower() for m in re.findall(search_pattern, error_line)]
-                if len(matches) > 1:
-                    print("Multiple possible fields found. Please type a number to exclude, or enter '0' to cancel.")
-                    for i in range(len(matches)):
-                        print(f"{i + 1}\t{matches[i]}")
-                    ans = int(input(""))
-                    if ans == 0:
-                        return result, num_fields, sql, query_url
-                    elif ans <= len(matches):
-                        match = matches[ans - 1]
-                        field = explore_name + '.' + match[len(explore_name) + 1:]
-                        excludes.append(field)
-                elif len(matches) == 1:
-                    match = matches[0]
-                    field = explore_name + '.' + match[len(explore_name) + 1:]
+    if result.strip().startswith('SQL Error'):
+        if continue_on_errors:
+            for field in field_set:
+                if field in result:
+                    if excludes == []:
+                        print(f"{colours['WARNING']}Errors found, continuing...{colours['END']}")
                     excludes.append(field)
-                else:
-                    return result, num_fields, sql, query_url
-                return check_fields(model_name, explore_name, print_data, continue_on_errors, excludes)
-    elif result is not None:       
-        if print_data:
-            print(f"Success for {num_fields} fields:\t{query_url}")
+                    return check_fields(model_name, explore_name, continue_on_errors, excludes)
         else:
-            print(f"Success for {num_fields} fields.")
+            print(f"{colours['FAIL']}{result}:\t{colours['END']}{colours['UNDERLINE']}{query_url}{colours['END']}")
+            return result, num_fields, sql, query_url
+    elif result is not None:       
+        print(f"{colours['OKGREEN']}Success for {num_fields} fields:\t{colours['END']}{colours['UNDERLINE']}{query_url}{colours['END']}\n")
         if excludes != []:
             excludes = list(set(excludes))
             excludes.sort()
-            print("Failures:")
+            print(f"{colours['FAIL']}Failures:{colours['END']}")
             for e in excludes:
-                print('\t', e)
+                print(f"{colours['FAIL']}\t{e}{colours['END']}")
     else:
         print(result)
     return result, num_fields, sql, query_url
 
 
-def check_fields_in_model(models=None, explores=None, print_data=False, continue_on_errors=False, excludes=None):
+def check_fields_in_model(models=None, explores=None, continue_on_errors=False, excludes=None):
     """Input the name of a model, and optionally, a list of explores to check against.
     If explores is None this will check the fields for all explores"""
     if models is None:
@@ -124,29 +110,27 @@ def check_fields_in_model(models=None, explores=None, print_data=False, continue
         elif isinstance(explores, str):
             explores = [explores]
         for explore in explores:
-            print(f"\nChecking fields for explore: {explore} in model: {model}")
+            print(f"{colours['BOLD']}\nChecking fields for explore: {colours['END']}{colours['OKBLUE']}{explore}{colours['END']}{colours['BOLD']} in model: {colours['END']}{colours['OKBLUE']}{model}{colours['END']}")
             print("_" * 79)
             if excludes is None:
                 excludes = []
-            _, _, _, _ = check_fields(model, explore, print_data, continue_on_errors, excludes)
+            _, _, _, _ = check_fields(model, explore, continue_on_errors, excludes)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("Put in the name of a model and a list of explores to check, or omit to check all explores.")
     parser.add_argument('--models', '-m', nargs='+', help='Pass in the name of a model or a space separated list of models')
     parser.add_argument('--explores', '-e', nargs='+', help='Pass in the name of an explore or a space separated list of explores')
-    parser.add_argument('--print', '-p', action='store_true', help='Print additional information for further debugging')
     parser.add_argument('--continue', '-c', action='store_true', help='Add this to continue on errors and surface a list. If excluded this will stop on the first error for each explore.')
     parser.add_argument('--dev', '-d', action='store_true', help='Include to compare to dev branch. Omit to use production.')
     parser.add_argument('--excludes', '-x',  nargs='+', help='Include a space separated list of fully-scoped (view.field) fields to ignore.')
     args = parser.parse_args()
     if args.dev:
         looker_client.update_session({'workspace_id': 'dev'})
-        print("Comparing to dev branch")
+        print(f"{colours['WARNING']}Comparing to dev branch{colours['END']}")
     else:
-        print("Comparing to prod branch")
+        print(f"{colours['HEADER']}Comparing to prod branch{colours['END']}")
     check_fields_in_model(models=args.models,
                           explores=args.explores,
-                          print_data=args.print,
                           continue_on_errors=getattr(args, 'continue'),
                           excludes=args.excludes)
